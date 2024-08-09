59efc308ac42e8fc50736ebfd1059cef28236172
HudrsonSilvaHS <155266342+sknnys@users.noreply.github.com>
2024-08-09 18:06:48 +0000

    adicionado estilos, função de remover usuarios cadastrados, que após removidos são adicionados a uma lista de usuarios removidos.

--- springbootviacodespace/src/main/java/com/hszindev/springbootviacodespace/controller/UserController.java
+++ springbootviacodespace/src/main/java/com/hszindev/springbootviacodespace/controller/UserController.java
@@ -3,6 +3,7 @@ package com.hszindev.springbootviacodespace.controller;
 import com.hszindev.springbootviacodespace.model.User;
 import com.hszindev.springbootviacodespace.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,14 +17,24 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User savedUser = userService.saveUser(user);
-        return ResponseEntity.ok(savedUser);
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        try {
+            User savedUser = userService.saveUser(user);
+            return ResponseEntity.ok(savedUser);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @GetMapping
     public ResponseEntity<List<User>> getUsers() {
         List<User> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- springbootviacodespace/src/main/java/com/hszindev/springbootviacodespace/service/UserService.java
+++ springbootviacodespace/src/main/java/com/hszindev/springbootviacodespace/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,10 +15,18 @@ public class UserService {
     private UserRepository userRepository;
 
     public User saveUser(User user) {
+        Optional<User> existingUser = userRepository.findByName(user.getName());
+        if (existingUser.isPresent()) {
+            throw new RuntimeException("User with this name already exists.");
+        }
         return userRepository.save(user);
     }
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    public void deleteUserById(Long userId) {
+        userRepository.deleteById(userId);
     }
 }
